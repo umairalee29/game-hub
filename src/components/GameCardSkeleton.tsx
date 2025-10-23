081631f2f88ea3de963c07381a1544e3ae57cede
umairalee29 <umair.alee93@gmail.com>
2025-10-23 13:05:32 +0200

    Refactor: remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={"300px"} borderRadius={10} overflow={"hidden"}>
-      <Skeleton height={"230px"} />
+    <Card>
+      <Skeleton height={"200px"} />
       <CardBody>
         <SkeletonText />
       </CardBody>
